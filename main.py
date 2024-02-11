42e663780870f66efe777756d08b0d0e16237e46
Michal Torma <michal.torma@nhm.uio.no>
2024-02-11 12:04:04 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import luigi
 #%%
 if __name__ == '__main__':
     settings_txt = os.getenv('SETTINGS')
+    print(settings_txt)
     settings = json.loads(settings_txt)
     tasks = [IPTCheck(ipt_url=x) for x in settings['ipt_servers']]
     luigi.build(tasks, local_scheduler=True)
